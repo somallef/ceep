c3d982d7a786873159fa6d4a8b73a003b2503652
somallef <allef.mizael@gmail.com>
2021-07-18 11:09:25 -0300

    Criação do projeto; commit inicial

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { Component } from 'react'
+import FormularioCadastro from './components/FormularioCadastro/index'
+import ListaDeNotas from './components/ListaDeNotas/index'
+import './assets/App.css'
+import './assets/index.css'
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      notas: [],
+    }
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto }
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas,
+    }
+    this.setState(novoEstado)
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    )
+  }
+}
+
+export default App
---
+++ src/components/CardNota/CardNota.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './estilo.css'
+
+class CardNota extends Component {
+  render() {
+    return (
+      <section className="card-nota">
+        <header>
+          <h3>{this.props.titulo}</h3>
+        </header>
+        <p>{this.props.texto}</p>
+      </section>
+    )
+  }
+}
+
+export default CardNota
